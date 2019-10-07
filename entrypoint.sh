432f64cbf3292ad4b2b5e2fac413c5f86889ea3e
tanmancan <contact@tkarimdesign.com>
2019-10-06 22:36:22 -0400

    Fix local path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ chmod 600 KEY_FILE
 
 ls -la
 
-scp -o StrictHostKeyChecking=no -i KEY_FILE -P "${INPUT_PORT}" -r "${INPUT_LOCAL_PATH}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_REMOTE_PATH}"
+scp -o StrictHostKeyChecking=no -i KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_LOCAL_PATH} "${INPUT_USER}@${INPUT_HOST}:${INPUT_REMOTE_PATH}"
 
 # cat KEY_FILE
 # echo $INPUT_USER
