20c073dbcefb206b8e725e9be7cc7ce131954472
tanmancan <contact@tkarimdesign.com>
2019-10-06 22:10:26 -0400

    Set correct permission for key file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo "${INPUT_KEY}" > KEY_FILE
 
+chmod 600 KEY_FILE
+
 ls -la
 
 scp -o StrictHostKeyChecking=no -i KEY_FILE -P "${INPUT_PORT}" -r "${INPUT_LOCAL_PATH}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_REMOTE_PATH}"
