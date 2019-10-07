918bfa8c4b8eb142e5d8551de36c9d5e813f0c20
tanmancan <contact@tkarimdesign.com>
2019-10-06 22:00:12 -0400

    Added scp command using input vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -l
 
 echo "$INPUT_KEY" > KEY_FILE
+
 ls -la
-cat KEY_FILE
-echo $INPUT_USER
-echo $INPUT_HOST
-echo $INPUT_PORT
-echo $INPUT_KEY
-echo $INPUT_LOCAL_PATH
-echo $INPUT_REMOTE_PATH
+
+scp -o StrictHostKeyChecking=no -i KEY_FILE -P "${INPUT_PORT}" -r "${INPUT_LOCAL_PATH}" "${INPUT_USER}@${INPUT_HOST}:${INPUT_REMOTE_PATH}"
+
+# cat KEY_FILE
+# echo $INPUT_USER
+# echo $INPUT_HOST
+# echo $INPUT_PORT
+# echo $INPUT_KEY
+# echo $INPUT_LOCAL_PATH
+# echo $INPUT_REMOTE_PATH
